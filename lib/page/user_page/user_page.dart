2922136a1dd65b63b0b42870e6e2cea752b5c603
danieledebernardini <debernardinidani@gmail.com>
2023-04-18 23:27:20 +0200

    Continued the changes to the calendar. On progress.

--- lib/page/user_page/user_page.dart
+++ lib/page/user_page/user_page.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
+
+import 'package:event/event.dart';
 import 'package:table_calendar/table_calendar.dart';
+
 
 /// Page displaying the summary of the events to which a user is subscribed as
 /// both host and guest.
 class UserPage extends StatefulWidget {
+	Map events = LinkedHashMap<DateTime, List<Event>>({
+		equals: isSameDay, hashCode: getHashCode,
+	});
 	
-	const UserPage({super.key});
+	UserPage({super.key});
 
 	@override
 	State<UserPage> createState() => _UserPageState();
 }
 
 class _UserPageState extends State<UserPage> {
+	var _calendarFormat = CalendarFormat.month;
 	var _selectedDay = DateTime.now();
 	var _focusedDay = DateTime.now();
+
+	/*
+	void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+		if (!isSameDay(_selectedDay, selectedDay)) {
+		setState(() {
+		_focusedDay = focusedDay;
+		_selectedDay = selectedDay;
+		_selectedEvents = _getEventsForDay(selectedDay);
+		});
+		}
+	}
+	*/
+
+	/// Returns the (eventually empty) list of events of a given day.
+	List<Event> _getEventsForDay(UserPage widget, DateTime day) {
+		return widget.events[day] ?? [];
+	}
 
 	@override
 	Widget build(BuildContext context) {
@@ -21,6 +45,7 @@ class _UserPageState extends State<UserPage> {
 			padding: const EdgeInsets.all(8),
 			children: <Widget>[
 				TableCalendar(
+					calendarFormat: _calendarFormat,
 					firstDay: DateTime(2006),
 					lastDay: DateTime(2036),
 					focusedDay: _focusedDay,
@@ -30,9 +55,16 @@ class _UserPageState extends State<UserPage> {
 					onDaySelected: (selectedDay, focusedDay) {
 						setState(() {
 							_selectedDay = selectedDay;
-							_focusedDay = focusedDay; // update `_focusedDay` here as well
+							_focusedDay = focusedDay;
 						});
 					},
+					onPageChanged: (focusedDay) {
+						_focusedDay = focusedDay;
+					},
+					eventLoader: (day) {
+						return _getEventsForDay(day);
+					},
+
 				),
 				Container(
 					height: 50,
