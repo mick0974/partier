eb35771b2accc127aa331ec429d85e2c0579b3f2
danieledebernardini <debernardinidani@gmail.com>
2023-07-20 20:38:50 +0200

    Migration to stateless widget for CreatePage.

--- lib/page/create_page/create_page.dart
+++ lib/page/create_page/create_page.dart
@@ -14,6 +14,191 @@ TO BE DONE
 - Add participants invitations;
 - Return error when it's not possible to add the event.
  */
+class CreatePage extends StatelessWidget {
+  CreatePage({super.key});
+
+  final db = Api();
+  final _formKey = GlobalKey<FormState>();
+
+  final DateFormat formatter = DateFormat('dd-MM-yyyy');
+
+  late String eventName;
+  String eventDescription = '';
+  DateTime eventDate = DateTime.now();
+  GeoPoint eventPlace = GeoPoint(0, 0);
+  String eventDress = '';
+
+  var nameController = TextEditingController();
+  var descrController = TextEditingController();
+  var dateController = TextEditingController();
+  var placeController = TextEditingController();
+
+  /// Allows to choose the date from a date picker instead of via keyboard.
+  /// After having selected a date, assigns its value to eventDate and fills the
+  /// corresponding field in the form.
+  void setDate(BuildContext context) async {
+    FocusScope.of(context).requestFocus(FocusNode()); //No keyboard
+
+    eventDate = (await showDatePicker(
+      context: context,
+      initialDate: eventDate,
+      firstDate: DateTime(2006),
+      lastDate: DateTime(2036),
+    ))!;
+
+    dateController.text = formatter.format(eventDate);
+  }
+
+  /// Given a prediction on an autocompleted address, eventually saves the
+  /// information in the eventPlace.
+  void setPlace(String? query) async {
+    if(query != null) {
+      List<Location> addresses = await locationFromAddress(
+          query, localeIdentifier: 'it');
+      var place = addresses.first;
+
+      print("Found (${place.latitude}, ${place.longitude})");
+
+      eventPlace = GeoPoint(place.latitude, place.longitude);
+    }
+  }
+
+  /// Attempts to add an event in the database.
+  /// After having retrieved the fields from the form, verifies whether or not
+  /// they are compatible with already existing events. Then either adds a new
+  /// event to the database or raises errors in the corresponding fields.
+  /*
+  TO BE IMPLEMENTED:
+  - Check on events in database
+   */
+  void tryAddEvent(BuildContext context) {
+    if (_formKey.currentState!.validate()) {
+      db.addEvent(eventName, eventDress, eventDescription, eventDate, eventPlace);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Added a party!')),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Error occurred, retry.')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const Key centerKey = ValueKey<String>('bottom-sliver-list');
+
+    return Scaffold(
+        body: Form(
+          child: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.center,
+                  child: const Text(
+                    'Create Event',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: "Event's name",
+                        ),
+                        validator: (String? value) {
+                          String? error;
+
+                          if (value == null || value.isEmpty) {
+                            error = 'Empty field not allowed.';
+                          } else {
+                            eventName = value;
+                          }
+
+                          return error;
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: "Event's Dress code",
+                        ),
+                        validator: (String? value) {
+                          String? error;
+
+                          if (value == null || value.isEmpty) {
+                            error = 'Empty field not allowed.';
+                          } else {
+                            eventDress = value;
+                          }
+
+                          return error;
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: "Event's description",
+                        ),
+                        validator: (String? value) {
+                          if (value != null && value.isNotEmpty) {
+                            eventDescription = value;
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        controller: dateController,
+                        decoration: const InputDecoration(
+                          labelText: "Event's date",
+                        ),
+                        validator: (String? value) {
+                          String? error;
+
+                          if (value == null || value.isEmpty) {
+                            error = 'Empty field not allowed.';
+                          } else {
+                            eventDate = formatter.parse(value);
+                          }
+
+                          return error;
+                        },
+                        onTap: () => setDate(context),
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: "Event's location",
+                        ),
+                        validator: (String? value) {
+                          String? error;
+
+                          if (value == null || value.isEmpty) {
+                            error = 'Empty field not allowed.';
+                          } else {
+                            setPlace(value);
+                          }
+
+                          return error;
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                TextButton.icon(
+                  label: const Text('Create!'),
+                  icon: const Icon(Icons.celebration),
+                  onPressed: () {
+                    tryAddEvent(context);
+                  },
+                )
+              ]
+          ),
+        )
+    );
+  }
+}
+
+/*
 class CreatePage extends StatefulWidget {
   const CreatePage({super.key});
 
@@ -199,6 +384,7 @@ class _CreatePage extends State<CreatePage> {
     );
   }
 }
+ */
 
 /// Class for the text fields in the form.
 // For the moment the check only evaluates if the string is empty/null and
