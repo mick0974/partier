5258911d9686ab489dfcc3c65319ec7cc2117cfe
danieledebernardini <debernardinidani@gmail.com>
2023-09-14 12:13:05 +0200

    Improvement of performances by reducing the call to setState() just when the refresh button is clicked.

--- lib/page/discover_page/discover_page.dart
+++ lib/page/discover_page/discover_page.dart
@@ -1,4 +1,3 @@
-import 'dart:collection';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -6,7 +5,6 @@ import 'package:partier/services/auth_service.dart';
 import '../event_widget/container/my_fancy_container.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:partier/services/api.dart';
-import '../../model/event.dart';
 
 
 /// Home page of the application.
@@ -25,126 +23,111 @@ class _DiscoverPage extends State<DiscoverPage> {
   var api = Api();
   var loginInfo = LoginInfo();  /// TODO: Not saving login info
 
-  List events = [];
+  late Future<List> events;
   List<Widget> eventContainers = [];
   bool firstUpdate = true;
   int counter = 0;
 
+  /// What do we need reset and firstUpdate?
+  Future<List> updateEventsList({bool reset = true}) async {
+    List tmp = [];
 
-  void createEventContainers(List ev) {
-    eventContainers = [];
-    for (var i = 0; i < min(ev.length,3); i++){
-      var tmpId = ev[i].keys.toList()[0];
-      var tmp = ev[i].values.toList()[0];
-      var date = DateTime.fromMillisecondsSinceEpoch(tmp["event_date"].seconds * 1000);
-
-      eventContainers.add(
-        Container(
-            alignment: Alignment.center,
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height*0.25,
-            child: MyFancyContainer(
-              id: tmpId,
-              title: tmp["name_event"],
-              date: "${date.day}-${date.month}-${date.year}",
-            )
-        )
-      );
-    }
-  }
-
-  void updateEventsList({bool reset = true}) {
     //https://firebase.google.com/docs/firestore/query-data/get-data
     counter++;
 
     if(reset) counter = 0;
     firstUpdate = !(counter > 1);
 
-    FirebaseFirestore.instance.collection("events")
+    CollectionReference ref = FirebaseFirestore.instance.collection('events');
+
+    QuerySnapshot eventsQuery = await ref
       .where("public", isEqualTo: true)
       .where("event_date", isGreaterThan: DateTime.now())
-      .get().then(
-        (querySnapshot) {
-          events = [];
+      .get();
 
-          for (var docSnapshot in querySnapshot.docs) {
-            setState(() {
-              events.add({docSnapshot.id: docSnapshot.data()});
-            });
-          }
-        },
-        onError: (e) => print("Error completing: $e"),
+    for (var doc in eventsQuery.docs) {
+      tmp.add({doc.id: doc.data()});
+    }
+
+    return tmp;
+  }
+
+  void createEventContainers(List ev) {
+    eventContainers = [];
+
+    for (var i = 0; i < min(ev.length, 3); i++) {
+      var tmpId = ev[i].keys.toList()[0];
+      var tmp = ev[i].values.toList()[0];
+      var date = DateTime.fromMillisecondsSinceEpoch(tmp["event_date"].seconds * 1000);
+
+      eventContainers.add(
+          Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height*0.25,
+              child: MyFancyContainer(
+                id: tmpId,
+                title: tmp["name_event"],
+                date: "${date.day}-${date.month}-${date.year}",
+              )
+          )
       );
+    }
+  }
 
-    createEventContainers(events);
+  @override
+  void initState() {
+    super.initState();
+
+    if(firstUpdate) events = updateEventsList(reset: false);
   }
 
   @override
   Widget build(BuildContext context) {
-    if(firstUpdate) updateEventsList(reset: false);
+    //if(firstUpdate) updateEventsList(reset: false);
     const Key centerKey = ValueKey<String>('bottom-sliver-list');
 
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: CustomScrollView(
-        center: centerKey,
-        slivers: <Widget>[
-          SliverAppBar(
-            flexibleSpace: SafeArea(
-              child: FilledButton.icon(
-                onPressed: updateEventsList,
-                icon: const Icon(Icons.update),
-                label: const Text('Tap to reload'),
-              )
-            ),
-          ),
-          SliverList(
-            key: centerKey,
-            delegate: SliverChildListDelegate(
-              eventContainers,
-            ),
-          ),
-        ],
-      ),
+      body: FutureBuilder<List>(
+        future: events,
+        builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+          if(snapshot.data != null) {
+            print('[Discover] ${snapshot.data}');
+            if (firstUpdate) createEventContainers(snapshot.data!);
+          } else {
+            eventContainers.add(const Text('No event available.'));
+          }
+
+          return CustomScrollView(
+            center: centerKey,
+            slivers: <Widget>[
+              SliverAppBar(
+                flexibleSpace: SafeArea(
+                    child: FilledButton.icon(
+                      onPressed: () {
+                        setState(() {
+                          updateEventsList();
+                          //createEventContainers(snapshot.data!);
+                        });
+                      },
+                      icon: const Icon(Icons.update),
+                      label: const Text('Tap to reload'),
+                    )
+                ),
+              ),
+              SliverList(
+                key: centerKey,
+                delegate: SliverChildListDelegate(
+                  eventContainers,
+                ),
+              ),
+            ],
+          );
+        },
+      )
     );
   }
 }
-
-/*
-class _UserInformationState extends State<UserInformation> {
-  final Stream<QuerySnapshot> _usersStream =
-      FirebaseFirestore.instance.collection('users').snapshots();
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: _usersStream,
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return const Text('Something went wrong');
-        }
-
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Text("Loading");
-        }
-
-        return ListView(
-          children: snapshot.data!.docs
-              .map((DocumentSnapshot document) {
-                Map<String, dynamic> data =
-                    document.data()! as Map<String, dynamic>;
-                return ListTile(
-                  title: Text(data['full_name']),
-                  subtitle: Text(data['company']),
-                );
-              })
-              .toList()
-              .cast(),
-        );
-      },
-    );
-  }
-}
- */
