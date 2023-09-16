559ee1b7b8d9361b3351b15b2de5fd3ec2ce5ebf
danieledebernardini <debernardinidani@gmail.com>
2023-09-16 16:07:12 +0200

    Improvement of performances using a stream for queries. Removed Slivers since the synchronization is now immediate.

--- lib/page/discover_page/discover_page.dart
+++ lib/page/discover_page/discover_page.dart
@@ -1,10 +1,6 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:partier/services/auth_service.dart';
 import '../event_widget/container/my_fancy_container.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:partier/services/api.dart';
 
 
 /// Home page of the application.
@@ -24,100 +20,58 @@ class _DiscoverPage extends State<DiscoverPage> {
   //var api = Api();
   //var loginInfo = LoginInfo();
 
-  late Future<List> events;
-  List<Widget> eventContainers = [];
+  final Stream<QuerySnapshot> _eventsStream =
+    FirebaseFirestore.instance.collection('events')
+    .where("public", isEqualTo: true)
+    .where("event_date", isGreaterThan: DateTime.now())
+    .snapshots();
 
-  /// What do we need reset and firstUpdate?
-  Future<List> updateEventsList() async {
-    List tmp = [];
+  /// Given a query snapshot, creates a list of widgets representing events
+  /// retrieved by the stream.
+  List<Widget> createEventContainers(AsyncSnapshot<QuerySnapshot> snapshot) {
+    return snapshot.data!.docs.map((DocumentSnapshot doc) {
+        Map<String, dynamic> data = doc.data()! as Map<String, dynamic>;
 
-    CollectionReference ref = FirebaseFirestore.instance.collection('events');
+        var date =
+          DateTime.fromMillisecondsSinceEpoch(data['event_date'].seconds * 1000);
 
-    QuerySnapshot eventsQuery = await ref
-      .where("public", isEqualTo: true)
-      .where("event_date", isGreaterThan: DateTime.now())
-      .get();
-
-    for (var doc in eventsQuery.docs) {
-      tmp.add({doc.id: doc.data()});
-    }
-
-    return tmp;
-  }
-
-  void createEventContainers(List ev) {
-    eventContainers = [];
-
-    for (var i = 0; i < min(ev.length, 3); i++) {
-      var tmpId = ev[i].keys.toList()[0];
-      var tmp = ev[i].values.toList()[0];
-      var date = DateTime.fromMillisecondsSinceEpoch(tmp["event_date"].seconds * 1000);
-
-      eventContainers.add(
-          Container(
-              alignment: Alignment.center,
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height*0.25,
-              child: MyFancyContainer(
-                id: tmpId,
-                title: tmp["name_event"],
-                date: "${date.day}-${date.month}-${date.year}",
-              )
-          )
-      );
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    events = updateEventsList();
+        return Container(
+            alignment: Alignment.center,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height*0.25,
+            child: MyFancyContainer(
+              id: doc.id,
+              title: data['name_event'],
+              date: "${date.day}-${date.month}-${date.year}",
+            )
+        );
+      })
+      .toList()
+      .cast();
   }
 
   @override
   Widget build(BuildContext context) {
-    //if(firstUpdate) updateEventsList(reset: false);
-    const Key centerKey = ValueKey<String>('bottom-sliver-list');
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: FutureBuilder<List>(
-        future: events,
-        builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
-          if(snapshot.data != null) {
-            createEventContainers(snapshot.data!);
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _eventsStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          Widget disc;
+
+          if (snapshot.hasError) {
+            disc = const Text('Something went wrong');
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            disc = const Text("Loading");
           } else {
-            eventContainers.add(const Text('No event available.'));
+            disc = ListView(
+              children: createEventContainers(snapshot),
+            );
           }
 
-          return CustomScrollView(
-            center: centerKey,
-            slivers: <Widget>[
-              SliverAppBar(
-                flexibleSpace: SafeArea(
-                    child: FilledButton.icon(
-                      onPressed: () {
-                        setState(() {
-                          updateEventsList();
-                          //createEventContainers(snapshot.data!);
-                        });
-                      },
-                      icon: const Icon(Icons.update),
-                      label: const Text('Tap to reload'),
-                    )
-                ),
-              ),
-              SliverList(
-                key: centerKey,
-                delegate: SliverChildListDelegate(
-                  eventContainers,
-                ),
-              ),
-            ],
-          );
+          return disc;
         },
       )
     );
